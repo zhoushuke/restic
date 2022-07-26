f7a30c14da7ed307f6a9a6f0990beb66428795ba
zhoushuke <zhoushuke@k2.com>
2022-07-26 14:09:39 +0800

    add binary

--- compile.sh
+++ compile.sh
@@ -1,2 +1,8 @@
-# linux
-go run build.go -o dist/restic --goos linux --goarch arm64
+# linux arm64
+go run build.go -o dist/restic.arm64 --goos linux --goarch arm64
+
+# linux amd64
+go run build.go -o dist/restic.amd64 --goos linux --goarch amd64
+
+# mac os
+go run build.go -o dist/restic.mac --goos darwin --goarch amd64
