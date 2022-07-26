97fed21b1d9e6bbb88bbda919c8da39d6309c698
zhoushuke <zhoushuke@k2.com>
2022-07-26 13:29:56 +0800

    add build cmd

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 # linux
-go run build.go -o dist/restic --goos linux --goarch arm64
+go run build.go -o dist/restic.arm64 --goos linux --goarch arm64
+
+# macos
+go run build.go -o dist/restic.mac --goos darwin --goarch amd64
